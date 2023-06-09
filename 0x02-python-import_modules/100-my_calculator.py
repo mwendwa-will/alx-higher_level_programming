022651df8749c244c620764c1d0c6e7ca764db2d
mwendwa-will <williammwendwa@gmail.com>
2023-06-09 03:42:18 +0300

    module

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,45 +1,19 @@
 #!/usr/bin/python3
-import calculator_1
-
-
-def calculator(argv):
-    n = len(argv) - 1
-    a = int(argv[1])
-    b = int(argv[3])
-    operator = argv[2]
-    finalcomp = 0
-    if n < 3:
-        <<<<<<< HEAD
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        return 1
-=======
-        print("./100-my_calculator.py <a> <operator> <b>")
-        break
-    return False
->>>>>>> 8aa0bb63feedf25d4fca6c0923043281297fcc6d
-    else:
-        if operator == " + ":
-            finalcomp = calculator_1.add(a, b)
-        elif operator == " - ":
-            finalcomp = calculator_1.sub(a, b)
-        elif operator == " * ":
-            finalcomp = calculator_1.mul(a, b)
-        elif operator == " / ":
-            finalcomp = calculator_1.div(a, b)
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            return 1
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, finalcomp))
-<<<<<<< HEAD
-    return False
-if __name__ == "__main__":
-    """import sys"""
-    """calculator(sys.argv)"""
-    return False
-=======
-
 
 if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
     import sys
-    calculator(sys.argv)
->>>>>>> 11abb7fa1ef7bde3ba265b541750fc4c98442ec9
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
